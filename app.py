2cb36fefd4a55b4c4f3fffdff5e201dce4ad89bf
Sakthi Mahendran R <133293074+404error-010@users.noreply.github.com>
2025-07-28 22:50:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
-import tensorflow as tf
 import pickle
 import os
 import subprocess
